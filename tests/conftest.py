4521912610f142952e144dd67745d92d8d70e625
Srikanth Krishnan <srikanthkrishnan@Srikanths-MacBook-Air.local>
2024-12-26 12:57:43 +0530

    hide api token

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,17 @@
 import pytest
+import os
+from dotenv import load_dotenv
 from faker import Faker
 from utils.config import Config
 from utils.api_client import APIClient
 
+load_dotenv()
 
 @pytest.fixture
 def api_client():
     # Initialize the ApiClient with a base URL and token
     base_url = Config.get_base_url()
-    token = "b6f4b4b2699e59e72c57c6fcb0f73484ee4a783d44e76fad4462cc5f6489b925"
+    token = os.getenv("API_TOKEN")
     return APIClient(base_url, token)
 
 @pytest.fixture
